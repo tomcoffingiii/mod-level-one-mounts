6a668d420630acd13b7e3e8f2092857a11cb3522
tomcoffingiii <tomcoffingiii@gmail.com>
2025-03-30 18:21:08 -0400

    Initial Upload
    Initial Upload

---
+++ level-twenty-mounts.sql
@@ -0,0 +1,15 @@
+use `acore_world`;
+
+update `item_template`
+set `RequiredLevel` = 20, `BuyPrice` = 100000, `SellPrice` = 25000
+where `entry` in (1132,2411,2414,5655,5656,5665,5668,5864,5872
+				,5873,8563,8588,8591,8592,8595,8629,8631,8632
+				,13321,13322,13331,13332,13333,15277,15290
+				,28481,28927,29220,29221,29222,29743,29744
+				,33224,33976,43599,46099,46100,46308,47100
+				,49283);
+
+update `npc_trainer`
+set `ReqLevel` = 20, `MoneyCost` = 100000
+where `ReqSkillLine` = 762
+and `ReqSkillRank` = 0;
